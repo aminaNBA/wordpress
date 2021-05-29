406111266522081c87f82a62dfb63a032ca29734
aminaNBA <71555958+aminaNBA@users.noreply.github.com>
2021-05-29 16:17:26 +0200

    MsQl

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'npak0382_aminaNBA' );
 define( 'DB_PASSWORD', 'aminaNBA123' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', '109.234.160.63' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
